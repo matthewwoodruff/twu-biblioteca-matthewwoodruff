9a52f86df1c4ad3145796f14c38720d729421395
Matthew Woodruff <matthew.woodruff@me.com>
2015-02-24 18:12:38 +0000

    Refactored the way Movie objects can be instantiated

--- src/com/twu/biblioteca/Movie.java
+++ src/com/twu/biblioteca/Movie.java
@@ -12,6 +12,10 @@ public class Movie {
 
     public Movie(final String title, final String year,
                  final String director, final Integer rating) {
+        if(title == null || title.isEmpty()) throw new IllegalArgumentException("title cannot be null or empty");
+        if(year == null || year.isEmpty()) throw new IllegalArgumentException("year cannot be null or empty");
+        if(director == null || director.isEmpty()) throw new IllegalArgumentException("director cannot be null or empty");
+        if(rating != null && (rating < 1 || rating > 10)) throw new IllegalArgumentException("rating must be between 1 and 10");
         this.title = title;
         this.year = year;
         this.director = director;
@@ -30,9 +34,16 @@ public class Movie {
         return director;
     }
 
-    public int getRating() {
+    public Integer getRating() {
         return rating;
     }
-    
+
+    public static Movie createUnratedMovie(String title, String year, String director) {
+        return new Movie(title, year, director, null);
+    }
+
+    public static Movie createRatedMovie(String title, String year, String director, int rating) {
+        return new Movie(title, year, director, rating);
+    }
 }
 
--- test/com/twu/biblioteca/MovieTests.java
+++ test/com/twu/biblioteca/MovieTests.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
@@ -37,4 +38,62 @@ public class MovieTests {
     public void testMovieHasARating() {
         assertThat(pulpFiction.getRating(), is(9));
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMovieCannotBeConstructedWithNullTitle() {
+        new Movie(null, "1994", "Quentin Tarantino", 9);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMovieCannotBeConstructedWithEmptyTitle() {
+        new Movie("", "1994", "Quentin Tarantino", 9);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMovieCannotBeConstructedWithNullYear() {
+        new Movie("Pulp Fiction", null, "Quentin Tarantino", 9);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMovieCannotBeConstructedWithEmptyYear() {
+        new Movie("Pulp Fiction", "", "Quentin Tarantino", 9);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMovieCannotBeConstructedWithNullDirector() {
+        new Movie("Pulp Fiction", "1994", null, 9);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMovieCannotBeConstructedWithEmptyDirector() {
+        new Movie("Pulp Fiction", "1994", "", 9);
+    }
+
+    @Test
+    public void testMovieCanBeConstructedWithNullRating() {
+        new Movie("Pulp Fiction", "1994", "Quentin Tarantino", null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMovieCannotBeConstructedWithRatingLessThan1() {
+        new Movie("Pulp Fiction", "1994", "Quentin Tarantino", -1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMovieCannotBeConstructedWithRatingGreaterThan10() {
+        new Movie("Pulp Fiction", "1994", "Quentin Tarantino", 11);
+    }
+
+    @Test
+    public void testCreateUnratedMovie() {
+        final Movie movie = Movie.createUnratedMovie("Pulp Fiction", "1994", "Quentin Tarantino");
+        assertThat(movie.getRating(), is(nullValue()));
+    }
+
+    @Test
+    public void testCreateRatedMovie() {
+        final Movie movie = Movie.createRatedMovie("Pulp Fiction", "1994", "Quentin Tarantino", 9);
+        assertThat(movie.getRating(), is(9));
+    }
+    
 }
