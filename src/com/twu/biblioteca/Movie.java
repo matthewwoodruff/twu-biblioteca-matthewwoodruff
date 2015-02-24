df04f26bbac2f64daaf48184d47b962c74518b2e
Matthew Woodruff <matthew.woodruff@me.com>
2015-02-24 18:32:54 +0000

    Movie objects now have an interface for checking in and out

--- src/com/twu/biblioteca/Movie.java
+++ src/com/twu/biblioteca/Movie.java
@@ -1,25 +1,35 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.exceptions.LibraryItemNotAvailableException;
+import com.twu.biblioteca.exceptions.LibraryItemNotCheckedOutException;
+
 /**
  * Created by Matt on 24/02/15.
  */
 public class Movie {
 
+    private final int id;
     private final String title;
     private final String year;
     private final String director;
     private final Integer rating;
+    private boolean checkedOut = false;
 
-    public Movie(final String title, final String year,
+    public Movie(final int id, final String title, final String year,
                  final String director, final Integer rating) {
         if(title == null || title.isEmpty()) throw new IllegalArgumentException("title cannot be null or empty");
         if(year == null || year.isEmpty()) throw new IllegalArgumentException("year cannot be null or empty");
         if(director == null || director.isEmpty()) throw new IllegalArgumentException("director cannot be null or empty");
         if(rating != null && (rating < 1 || rating > 10)) throw new IllegalArgumentException("rating must be between 1 and 10");
+        this.id = id;
         this.title = title;
         this.year = year;
         this.director = director;
         this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getTitle() {
@@ -38,12 +48,30 @@ public class Movie {
         return rating;
     }
 
-    public static Movie createUnratedMovie(String title, String year, String director) {
-        return new Movie(title, year, director, null);
+    public static Movie createUnratedMovie(int id, String title, String year, String director) {
+        return new Movie(id, title, year, director, null);
     }
 
-    public static Movie createRatedMovie(String title, String year, String director, int rating) {
-        return new Movie(title, year, director, rating);
+    public static Movie createRatedMovie(int id, String title, String year, String director, int rating) {
+        return new Movie(id, title, year, director, rating);
+    }
+
+    public boolean isCheckedOut() {
+        return checkedOut;
+    }
+
+    public boolean isAvailable() {
+        return !checkedOut;
+    }
+
+    public void checkOut() throws LibraryItemNotAvailableException {
+        if(isCheckedOut()) throw new LibraryItemNotAvailableException();
+        checkedOut = true;
+    }
+
+    public void checkIn() throws LibraryItemNotCheckedOutException {
+        if(isAvailable()) throw new LibraryItemNotCheckedOutException();
+        checkedOut = false;
     }
 }
 
--- test/com/twu/biblioteca/MovieTests.java
+++ test/com/twu/biblioteca/MovieTests.java
@@ -1,5 +1,7 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.exceptions.LibraryItemNotAvailableException;
+import com.twu.biblioteca.exceptions.LibraryItemNotCheckedOutException;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,7 +18,7 @@ public class MovieTests {
 
     @Before
     public void setup() {
-        pulpFiction = new Movie("Pulp Fiction", "1994", "Quentin Tarantino", 9);
+        pulpFiction = new Movie(1, "Pulp Fiction", "1994", "Quentin Tarantino", 9);
     }
 
     @Test
@@ -39,61 +41,102 @@ public class MovieTests {
         assertThat(pulpFiction.getRating(), is(9));
     }
 
+    @Test
+    public void testMovieHasAnId() {
+        assertThat(pulpFiction.getId(), is(1));
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void testMovieCannotBeConstructedWithNullTitle() {
-        new Movie(null, "1994", "Quentin Tarantino", 9);
+        new Movie(1, null, "1994", "Quentin Tarantino", 9);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testMovieCannotBeConstructedWithEmptyTitle() {
-        new Movie("", "1994", "Quentin Tarantino", 9);
+        new Movie(1, "", "1994", "Quentin Tarantino", 9);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testMovieCannotBeConstructedWithNullYear() {
-        new Movie("Pulp Fiction", null, "Quentin Tarantino", 9);
+        new Movie(1, "Pulp Fiction", null, "Quentin Tarantino", 9);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testMovieCannotBeConstructedWithEmptyYear() {
-        new Movie("Pulp Fiction", "", "Quentin Tarantino", 9);
+        new Movie(1, "Pulp Fiction", "", "Quentin Tarantino", 9);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testMovieCannotBeConstructedWithNullDirector() {
-        new Movie("Pulp Fiction", "1994", null, 9);
+        new Movie(1, "Pulp Fiction", "1994", null, 9);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testMovieCannotBeConstructedWithEmptyDirector() {
-        new Movie("Pulp Fiction", "1994", "", 9);
+        new Movie(1, "Pulp Fiction", "1994", "", 9);
     }
 
     @Test
     public void testMovieCanBeConstructedWithNullRating() {
-        new Movie("Pulp Fiction", "1994", "Quentin Tarantino", null);
+        new Movie(1, "Pulp Fiction", "1994", "Quentin Tarantino", null);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testMovieCannotBeConstructedWithRatingLessThan1() {
-        new Movie("Pulp Fiction", "1994", "Quentin Tarantino", -1);
+        new Movie(1, "Pulp Fiction", "1994", "Quentin Tarantino", -1);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testMovieCannotBeConstructedWithRatingGreaterThan10() {
-        new Movie("Pulp Fiction", "1994", "Quentin Tarantino", 11);
+        new Movie(1, "Pulp Fiction", "1994", "Quentin Tarantino", 11);
     }
 
     @Test
     public void testCreateUnratedMovie() {
-        final Movie movie = Movie.createUnratedMovie("Pulp Fiction", "1994", "Quentin Tarantino");
+        final Movie movie = Movie.createUnratedMovie(1, "Pulp Fiction", "1994", "Quentin Tarantino");
         assertThat(movie.getRating(), is(nullValue()));
     }
 
     @Test
     public void testCreateRatedMovie() {
-        final Movie movie = Movie.createRatedMovie("Pulp Fiction", "1994", "Quentin Tarantino", 9);
+        final Movie movie = Movie.createRatedMovie(1, "Pulp Fiction", "1994", "Quentin Tarantino", 9);
         assertThat(movie.getRating(), is(9));
     }
-    
+
+    @Test
+    public void testMovieHasCheckedOutIndication() {
+        assertThat(pulpFiction.isCheckedOut(), is(false));
+    }
+
+    @Test
+    public void testMovieHasAvailableIndication() {
+        assertThat(pulpFiction.isAvailable(), is(true));
+    }
+
+    @Test
+    public void testMovieCanBeCheckedOut() throws LibraryItemNotAvailableException {
+        pulpFiction.checkOut();
+        assertThat(pulpFiction.isCheckedOut(), is(true));
+        assertThat(pulpFiction.isAvailable(), is(false));
+    }
+
+    @Test(expected = LibraryItemNotAvailableException.class)
+    public void testCheckingOutAnUnavailableMovieThrowsAnException() throws LibraryItemNotAvailableException {
+        pulpFiction.checkOut();
+        pulpFiction.checkOut();
+    }
+
+    @Test
+    public void testMovieCanBeCheckedIn() throws LibraryItemNotAvailableException, LibraryItemNotCheckedOutException {
+        pulpFiction.checkOut();
+        pulpFiction.checkIn();
+        assertThat(pulpFiction.isCheckedOut(), is(false));
+        assertThat(pulpFiction.isAvailable(), is(true));
+    }
+
+    @Test(expected = LibraryItemNotCheckedOutException.class)
+    public void testCheckingInAMovieThatHasntBeenCheckedOutThrowsAnException() throws LibraryItemNotCheckedOutException {
+        pulpFiction.checkIn();
+    }
+
 }
